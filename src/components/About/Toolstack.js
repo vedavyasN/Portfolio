c35a4be987b3bf36e0b497c9f32bf6666449a51d
vedavyas N <139968432+vedavyasN@users.noreply.github.com>
2025-07-27 20:15:14 +0530

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -2,10 +2,6 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
-  SiPostman,
-  SiSlack,
-  SiVercel,
-  SiMacos,
 } from "react-icons/si";
 
 function Toolstack() {
