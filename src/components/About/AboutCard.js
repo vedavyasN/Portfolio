0f335360d94bcab46c824718a23784209f152f6c
vedavyas N <139968432+vedavyasN@users.noreply.github.com>
2025-07-27 20:21:03 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,8 +8,8 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Vedavyas</span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone,I am <span className="purple">Vedavyas</span>
+            from <span className="purple">Ballari,India.</span>
             <br />
            I have completed my B.Tech in Artificial Intelligence and Machine Learning from BITM College. 
             <br />
