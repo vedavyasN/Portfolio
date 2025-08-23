4c8b6a7acf804c5f9c7da512fd6f38c1d2978c58
vedavyas N <139968432+vedavyasN@users.noreply.github.com>
2025-08-23 16:26:57 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,30 +21,6 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
 
-        {/* ✅ Put Cols inside Row */}
-        <Row>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={bitsOfCode}
-              isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page built with Next.js and Tailwind CSS. It takes content from markdown files and renders it using Next.js. Supports dark mode and easy blogging using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor built with React.js. Supports HTML, CSS, and JS code with instant preview. Also supports markdown README editing with toolbar and auto save using Local Storage."
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"
-            />
-          </Col>
-        </Row>
       </Container>
     </Container>
   );
